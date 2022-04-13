d4e9ecec03978e2b0cbca8521c3c3a5b2af1fdeb
enniohenrique <ennioh@icloud.com>
2022-04-13 13:44:04 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@ function soma() {
 }
 
 function subtrai() {
+    if(document.getElementById('resultado').innerHTML > 0)
+    {
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = eval(resultado) - 1;   
+    document.getElementById('resultado').innerHTML = eval(resultado) - 1;  
+    } 
+    else {
+        document.getElementById('resultado').innerHTML = "0";
+    }
 }
 function clean() {
     document.getElementById('resultado').innerHTML = "0";
